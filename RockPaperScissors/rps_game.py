95d189aa2ca669dddccf172737f4c74d5d599d3c
Ramkumar Sakthivel <ramkumarsakthivel93@gmail.com>
2020-07-05 21:17:14 +0200

    add error handling

--- RockPaperScissors/rps_game.py
+++ RockPaperScissors/rps_game.py
@@ -11,28 +11,34 @@ choices = ['R','S','P']
 
 def decide(player_choice,computer_choice):
     if player_choice==computer_choice:
-        return 'Its a tie :|'
+        return 'tie'
     elif choices.index(player_choice)+1==choices.index(computer_choice) or choices.index(player_choice)-choices.index(computer_choice)==2:
         return 'Congrats! You won :)'
     else:
         return 'Unlucky! Computer won :('
 
 def rps():
-    # Get players choise
-    player_choice = input('Choose R(rock) or P(paper) or S(scissor): ') 
-    print('Your choise:',player_choice)
-    while player_choice not in choices:
-        print('Please make a valid choise !')
-        player_choice = input('Choose R(rock) or P(paper) or S(scissor): ')
+    check_tie = False #Flag
 
-    # Get computers choise
-    computer_choice = random.choice(choices)
-    print("Computer's choice:",computer_choice)
-
-    # Get decision
-    decision = decide(player_choice,computer_choice)
+    # Play until its not a tie
+    while not check_tie:
+        # Get players choise
+        player_choice = input('Choose R(rock) or P(paper) or S(scissor): ') 
+        print('Your choise:',player_choice)
+        # If user does invalid selection
+        while player_choice not in choices:
+            print('Please make a valid choise !')
+            player_choice = input('Choose R(rock) or P(paper) or S(scissor): ')
+        # Get computers choise
+        computer_choice = random.choice(choices)
+        print("Computer's choice:",computer_choice)
+        decision = decide(player_choice,computer_choice)
+        if decision=='tie':
+            print('Its a tie :| Try again !')
+        else:
+            check_tie = True
     print(decision)
-
+    
     # Play again?
     again = input('Play again y or n ?: ')
     if again=='y':
