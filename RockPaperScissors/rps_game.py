89fb65ac2f7719598e22f763ce3f203c235ce437
Ramkumar Sakthivel <ramkumarsakthivel93@gmail.com>
2020-07-05 20:13:14 +0200

    remove bug

--- RockPaperScissors/rps_game.py
+++ RockPaperScissors/rps_game.py
@@ -9,45 +9,43 @@ print('Hello',player_name,'!, Lets begin...')
 # Available choices
 choices = ['R','S','P']
 
-
 def decide(player_choice,computer_choice):
     if player_choice==computer_choice:
-        print('Its a tie. Try again !')
-        main()
+        return 'Its a tie :|'
     elif choices.index(player_choice)+1==choices.index(computer_choice) or choices.index(player_choice)-choices.index(computer_choice)==2:
         return 'Congrats! You won :)'
     else:
         return 'Unlucky! Computer won :('
 
-
-
-
-def main():
+def rps():
     # Get players choise
-    player_choice = input('Choose R(rock) or P(paper) or S(scissor)')
+    player_choice = input('Choose R(rock) or P(paper) or S(scissor): ') 
     print('Your choise:',player_choice)
-    if player_choice not in choices:
+    while player_choice not in choices:
         print('Please make a valid choise !')
-        main()
+        player_choice = input('Choose R(rock) or P(paper) or S(scissor): ')
 
     # Get computers choise
     computer_choice = random.choice(choices)
     print("Computer's choice:",computer_choice)
 
+    # Get decision
     decision = decide(player_choice,computer_choice)
-
     print(decision)
 
-    again = input('Play again y or n ?')
-
+    # Play again?
+    again = input('Play again y or n ?: ')
     if again=='y':
-        main()
-    else:
+        rps()
+    elif again=='n':
         print('Goodbye!')
+        pass
+    else:
+        print('Invalid choice, Goodbye!')
+        return 0
 
-
-
-main()
+# Call function
+rps()
 
 
 
